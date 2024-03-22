72a72324fe07981fbd53a082dc798d4ab0b9c1d4
Yusuf <mirlanuulu.04@gamil.com>
2024-03-22 14:52:13 +0600

    update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,8 +3,7 @@ import car from '../../assets/png/car.png'
 
 function Home() {
   return (
-    <div className='w-full flex flex-wrap items-center justify-around'  style={{ backgroundImage: `url(${car})` }}>
-
+    <div className='w-full flex flex-wrap items-center justify-around bg-cover'  style={{ backgroundImage: `url(${car})` }}>
 
         <div className='container flex justify-around'>
 
@@ -17,43 +16,7 @@ function Home() {
           <button className='text-red-500'>=</button>
 
         </div>
-
-        
-        <div className='bg-cover bg-center min-h-screen flex flex-col items-center justify-center'>
-        
-        <h1 className='text-5xl font-bold text-red-500 mt-20'>CAR MUSIC</h1>
-        <h1 className='text-7xl text-red-500'>. . . .</h1>
-        <p className='text-white text-center max-w-md mt-20'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur dolores sapiente, fugit reiciendis quas optio harum! Asperiores aspernatur ullam quasi aliquam alias a animi, voluptatibus consequatur quas, ipsum provident suscipit.
-        </p>
-        
-        
-        <div className=' container grid grid-cols-3 w-full mt-[170px]'>
-
-          <div className='p-4 border border-gray-300 w-full'>
-            <h2 className='text-red-500 font-bold text-xl'>Адрес:</h2>
-            <p className='text-white'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae temporibus alias quasi.
-            </p>
-          </div>
     
-          <div className='p-4 border border-gray-300'>
-            <h2 className='text-red-500 font-bold text-xl'>Адрес:</h2>
-            <p className='text-white'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae temporibus alias quasi.
-            </p>
-          </div>
-    
-          <div className='p-4 border border-gray-300'>
-            <h2 className='text-red-500 font-bold text-xl'>Адрес:</h2>
-            <p className='text-white'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae temporibus alias quasi.
-            </p>
-          </div>
-          
-        </div>
-
-      </div>    
     </div>
   );
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
+import car from '../../assets/png/car.png'
 
 function Home() {
   return (
-    <div className='bg-black text-white'>
+    <div className='text-white'>
+
+
+      <div className='bg-cover bg-center min-h-screen flex flex-col items-center justify-center'  style={{ backgroundImage: `url(${car})`}}>
+        
+        <h1 className='text-5xl font-bold text-red-500 mt-20'>CAR MUSIC</h1>
+        <h1 className='text-7xl text-red-500'>. . . .</h1>
+        <p className='text-white text-center max-w-md mt-20'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur dolores sapiente, fugit reiciendis quas optio harum! Asperiores aspernatur ullam quasi aliquam alias a animi, voluptatibus consequatur quas, ipsum provident suscipit.
+        </p>
       
-      <div className='container flex'>
+      
+        <div className=' container grid grid-cols-3 w-full mt-[370px]'>
+
+          <div className='p-4 border border-gray-300 w-full'>
+            <h2 className='text-red-500 font-bold text-xl'>Адрес:</h2>
+            <p className='text-white'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae temporibus alias quasi.
+            </p>
+          </div>
+
+          <div className='p-4 border border-gray-300'>
+            <h2 className='text-red-500 font-bold text-xl'>Адрес:</h2>
+            <p className='text-white'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae temporibus alias quasi.
+            </p>
+          </div>
+
+          <div className='p-4 border border-gray-300'>
+            <h2 className='text-red-500 font-bold text-xl'>Адрес:</h2>
+            <p className='text-white'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae temporibus alias quasi.
+            </p>
+          </div>
+        
+        </div>
+
+      </div>
+
+      
+      <div className='container w-[1900px] flex mx-auto bg-neutral-900'>
       
         <div className='container mx-auto p-[180px]'>
           <h1 className='uppercase'>наши достижения от кубка россии до чемпионатов мира</h1>
@@ -35,7 +74,7 @@ function Home() {
           
           </div>
 
-          <div className='mt-[150px]'>
+          <div className='mt-[150px] mr-[250px]'>
 
             <div className='p-4 border'>
               <h1 className='text-xs font-bold'>#1</h1>
@@ -59,7 +98,28 @@ function Home() {
           
           </div>
         
-        </div>
+          <div>
+
+          <div>
+
+            <div>
+              <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At saepe obcaecati facilis dolorem modi, facere optio impedit. Perspiciatis consequatur adipisci tempora hic labore ut corrupti, incidunt nisi vitae ducimus ipsa?</p>
+            </div>
+            <div>
+              <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At saepe obcaecati facilis dolorem modi, facere optio impedit. Perspiciatis consequatur adipisci tempora hic labore ut corrupti, incidunt nisi vitae ducimus ipsa?</p>
+            </div>
+            <div>
+              <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At saepe obcaecati facilis dolorem modi, facere optio impedit. Perspiciatis consequatur adipisci tempora hic labore ut corrupti, incidunt nisi vitae ducimus ipsa?</p>
+            </div>
+
+          </div>
+
+          </div>
+
+      </div>
 
     </div>
   );
